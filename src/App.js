c5ec8ac2e7e718fe30f7af1463e84c3b5c1a449a
marina <marinamsm@live.com>
2020-02-25 21:23:44 -0300

    All changes in App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './contents/Home';
 
 function App() {
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <Navbar />
+        <Route exact path="/">
+          <Home />
+        </Route>
       </div>
     </Router>
   );
