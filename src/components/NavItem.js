e808f0cf0c6d74e694806dc7fee98811c5fdd7ca
marina <marinamsm@live.com>
2020-02-25 21:23:26 -0300

    Bug fix

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -2,18 +2,10 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Navitem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            item: props.item,
-            tolink: props.tolink,
-            activec: props.activec
-        }
-    }
 
     render() {
         return (
-            <li>
+            <li id={this.props.item}>
                 <Link to={this.props.tolink} onClick={this.props.activec.bind(this,this.props.item)}>
                     {this.props.item}
                 </Link>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,20 @@ class Navbar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            NavItemActive:'',
-            NavItemId: []
+            NavItemActive:''
         }
+        this.activeitem = this.activeitem.bind(this);
     }
 
     activeitem(x) {
-        if(this.state.NavItemId.length > 0) {
+        console.log('element id is: ', x);
+        if(this.state.NavItemActive.length > 0) {
             document.getElementById(this.state.NavItemActive).classList.remove('active');
+        } else {
+            this.setState({'NavItemActive': x}, () => {
+                document.getElementById(this.state.NavItemActive).classList.add('active');
+            });
         }
-        this.setState({'NavItemActive': x}, () => {
-            document.getElementById(this.state.NavItemActive).classList.add('active');
-        });
     };
 
     render() {
