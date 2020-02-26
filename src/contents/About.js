02cbac8fe2d129ea4de2c68b1c446e1c9ce2d106
marina <marinamsm@live.com>
2020-02-25 22:27:38 -0300

    About section with no description

---
+++ src/contents/About.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+class About extends Component {
+    render() {
+        return (
+            <div className="condiv">
+                <h1 className="subtopic">About Me</h1>
+                <h4>Hey there,</h4>
+                <h1>I'm Abdul Wahid Naafi</h1>
+                <h3>Full Stack Web <u>Developer</u> | UI/UX <u>Designer</u></h3>
+                <br></br>
+                <p>A description will be provided soon. Stay tuned!!
+                </p>
+            </div>
+        )
+    }
+}
+export default About
