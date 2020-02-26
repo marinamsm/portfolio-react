ae85d97952905ba300d2de2f7117b31a1cdb9419
marina <marinamsm@live.com>
2020-02-25 21:22:16 -0300

    Social networks

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,1 +1,12 @@
-import react, { Component } from 'react';
+import React, { Component } from 'react';
+class Social extends Component {
+    render() {
+        return (
+        <div class="social">
+            <a href="https://github.com/marinamsm" target="_blank"><i class="fab fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/marina-de-souza-mendes-63852957/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+        </div>
+        )
+    }
+}
+export default Social
